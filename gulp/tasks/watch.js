b4824db5bee955a4aa6eb72cd66e6d98d558e6ef
Zac Tolley <zac@thetolleys.com>
2017-03-07 15:29:30 +0000

    Simplify header to remove crown and fix position and spacing of title. Do not apply revision when running in develop mode.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ const paths = require('../paths')
 const gulpSequence = require('gulp-sequence')
 
 gulp.task('watch', (done) => {
-  gulpSequence('build', 'serve', 'browserSync', () => {
+  gulpSequence('clean', 'nunjucks', 'sass', 'css', 'webpack', 'images', 'serve', 'browserSync', () => {
     gulp.watch([`${paths.projectDir}/src/components/**/*.js`, `${paths.projectDir}/src/javascripts/**/*.js`], ['webpack'])
     gulp.watch(`${paths.projectDir}/src/**/*.scss`, ['sass', 'css'])
     gulp.watch(`${paths.imagesSource}/**/*`, ['images'])
